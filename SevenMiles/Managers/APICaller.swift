75e22a629c0e47641e767be992c6558064230138
Sean Blake <sean.blake97@outlook.com>
2021-10-09 12:44:13 -0400

    Created the code for the Managers

--- SevenMiles/Managers/APICaller.swift
+++
@@ -1,8 +0,0 @@
-//
-//  APICaller.swift
-//  APICaller
-//
-//  Created by SEAN BLAKE on 10/8/21.
-//
-
-import Foundation
--- SevenMiles/Managers/AuthenticationManager.swift
+++ SevenMiles/Managers/AuthenticationManager.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+final class AuthManager {
+    public static let shared = AuthManager()
+    
+    private init() {}
+    
+    enum SignInMethod {
+        case email
+        case facebook
+        case google
+//        case apple
+//        case phone
+    }
+    
+    // Public
+    
+    public func signIn(with method: SignInMethod) {
+        
+    }
+    
+    public func signOut() {
+        
+    }
+}
