8cb62b68cb0e92dacb8c9fcd6d7869c6b144e55c
Sean Blake <sean.blake97@outlook.com>
2021-10-08 20:30:00 -0400

    first commit

---
+++ SevenMiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SevenMiles
+//
+//  Created by SEAN BLAKE on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
