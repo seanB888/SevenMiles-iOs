26038825113e20a79224b834e554969f5a6b398b
Sean Blake <sean.blake97@outlook.com>
2021-10-09 12:11:11 -0400

    Added tabBar with icons and install SDWebImages

--- SevenMiles/Controllers/Core/TabBarViewController.swift
+++ SevenMiles/Controllers/Core/TabBarViewController.swift
@@ -11,19 +11,35 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setUpController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpController() {
+        let home = HomeViewController()
+        let explore = ExploreViewController()
+        let camera = CameraViewController()
+        let notification = NotificationViewController()
+        let profile = ProfileViewController()
+        
+        home.title = "Home"
+        explore.title = "Explore"
+        // camera.title = "Create"
+        notification.title = "DM'S"
+        profile.title = "Profile"
+        
+        
+        let nav1 = UINavigationController(rootViewController: home)
+        let nav2 = UINavigationController(rootViewController: explore)
+        let nav3 = UINavigationController(rootViewController: notification)
+        let nav4 = UINavigationController(rootViewController: profile)
+        
+        nav1.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "safari"), tag: 2)
+        camera.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "plus.square"), tag: 3)
+        nav3.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "mail"), tag: 4)
+        nav4.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person"), tag: 5)
+        
+        setViewControllers([nav1, nav2, camera, nav3, nav4], animated: false)
     }
-    */
-
 }
