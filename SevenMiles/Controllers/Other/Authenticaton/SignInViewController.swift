beaf0f134db1fed1a9ffef1d5c87f06c842674d2
Sean Blake <sean.blake97@outlook.com>
2021-10-19 22:38:40 -0400

    SIgnUp Screen

--- SevenMiles/Controllers/Other/Authenticaton/SignInViewController.swift
+++ SevenMiles/Controllers/Other/Authenticaton/SignInViewController.swift
@@ -7,14 +7,36 @@
 
 import UIKit
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UITextViewDelegate  {
     
     public var completion: (() -> Void)?
+    
+    // MARK: - Logo
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        imageView.image = UIImage(named: "logo")
+        return imageView
+    }()
+    
+    private let emailField = AuthField(type: .email)
+    private let passwordField = AuthField(type: .password)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Sign In"
+        view.addSubview(logoImageView)
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        emailField.delegate = self
+        passwordField.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
 
 }
