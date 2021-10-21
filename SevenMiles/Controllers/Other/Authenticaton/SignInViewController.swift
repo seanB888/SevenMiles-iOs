180bab9ec46dc326bf2f0c67adc604f9234cb7bf
Sean Blake <sean.blake97@outlook.com>
2021-10-20 23:36:09 -0400

    Setup the Signin View and added the logic to the textFields and the buttons

--- SevenMiles/Controllers/Other/Authenticaton/SignInViewController.swift
+++ SevenMiles/Controllers/Other/Authenticaton/SignInViewController.swift
@@ -5,9 +5,10 @@
 //  Created by SEAN BLAKE on 10/8/21.
 //
 
+import SafariServices
 import UIKit
 
-class SignInViewController: UIViewController, UITextViewDelegate  {
+class SignInViewController: UIViewController, UITextFieldDelegate {
     
     public var completion: (() -> Void)?
     
@@ -22,21 +23,113 @@ class SignInViewController: UIViewController, UITextViewDelegate  {
     }()
     
     private let emailField = AuthField(type: .email)
+    
     private let passwordField = AuthField(type: .password)
+    
+    private let signInButton = AuthButton(type: .signIn, title: nil)
+    private let forgetPassword = AuthButton(type: .plain, title: "Forgot Your Password")
+    private let signUpButton = AuthButton(type: .signUp, title: "New User? Create Account")
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Sign In"
+        addSubViews()
+        configureField()
+        configureButtons()
+    }
+    
+    func configureButtons() {
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        signUpButton.addTarget(self, action: #selector(didTapSignUp), for: .touchUpInside)
+        forgetPassword.addTarget(self, action: #selector(didTapForgetPassword), for: .touchUpInside)
+    }
+    
+    func configureField() {
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.width, height: 50))
+        toolBar.items = [
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(didTapkeyboardDone))
+        ]
+        toolBar.sizeToFit()
+        emailField.inputAccessoryView = toolBar
+        passwordField.inputAccessoryView = toolBar
+    }
+    
+    func addSubViews() {
         view.addSubview(logoImageView)
         view.addSubview(emailField)
         view.addSubview(passwordField)
-        emailField.delegate = self
-        passwordField.delegate = self
+        view.addSubview(signInButton)
+        view.addSubview(signUpButton)
+        view.addSubview(forgetPassword)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
+        let imageSize: CGFloat = 100
+        // logo
+        logoImageView.frame = CGRect(x: (view.width - imageSize)/2, y: view.safeAreaInsets.top + 5, width: imageSize, height: imageSize)
+        
+        emailField.frame = CGRect(x: 20, y: logoImageView.bottom+20, width: view.width-40, height: 55)
+        passwordField.frame = CGRect(x: 20, y: emailField.bottom+15, width: view.width-40, height: 55)
+        
+        signInButton.frame = CGRect(x: 20, y: passwordField.bottom+20, width: view.width-40, height: 55)
+        signUpButton.frame = CGRect(x: 20, y: signInButton.bottom+20, width: view.width-40, height: 55)
+        forgetPassword.frame = CGRect(x: 20, y: signUpButton.bottom+20, width: view.width-40, height: 55)
+        
+    }
+    
+    // Actions
+    @objc func didTapSignIn() {
+        didTapkeyboardDone()
+        
+        guard let email = emailField.text,
+              let password = passwordField.text,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.count >= 8 else {
+                  
+                  let alert = UIAlertController(title: "Yu messup, check dat again", message: "Di Password or the Username wrong. Fix up dat and try again!", preferredStyle: .alert)
+                  alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                  present(alert, animated: true)
+            return
+              }
+        
+        AuthManager.shared.signIn(with: email, password: password) { loggedIn in
+            if loggedIn {
+                // dismiss sign in
+            } else {
+                // show error
+            }
+        }
+    }
+    
+    @objc func didTapSignUp() {
+        didTapkeyboardDone()
+        let vc = SignUpViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func didTapForgetPassword() {
+        didTapkeyboardDone()
+        guard let url = URL(string: "https://5fourlab.com") else {
+            return
+        }
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
+    
+    @objc func didTapkeyboardDone() {
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
     }
 
 }
