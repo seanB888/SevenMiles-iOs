caab8d34c8d89e353fef429ab770cdebaaac7bd6
Sean Blake <sean.blake97@outlook.com>
2021-10-20 23:36:09 -0400

    Setup the Signin View and added the logic to the textFields and the buttons

--- SevenMiles/Controllers/Other/Authenticaton/AuthButton.swift
+++ SevenMiles/Controllers/Other/Authenticaton/AuthButton.swift
@@ -10,15 +10,48 @@ import UIKit
 class AuthButton: UIButton {
     
     enum ButtonType {
-        case SignIn
-        case SignUp
-        case Plain
+        case signIn
+        case signUp
+        case plain
+        
+        var title: String {
+            switch self {
+            case .signIn: return "Sign In"
+            case .signUp: return "Sign Up"
+            case .plain: return "_"
+            }
+        }
     }
     
-    let type = ButtonType
+    let type: ButtonType
     
-    init(type: ButtonType) {
+    init(type: ButtonType, title: String?) {
         self.type = type
         super.init(frame: .zero)
+        if let title = title {
+            setTitle(title, for: .normal)
+        }
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func configure() {
+        if type != .plain {
+            setTitle(type.title, for: .normal)
+        }
+        setTitleColor(.white, for: .normal)
+        switch type {
+        case .signIn: backgroundColor = .systemPink
+        case .signUp: backgroundColor = .systemOrange
+        case .plain:
+            setTitleColor(.link, for: .normal)
+            backgroundColor = .clear
+        }
+        titleLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        layer.cornerRadius = 25
+        layer.masksToBounds = true
     }
 }
