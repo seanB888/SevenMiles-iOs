3ec293d752a2c8127c85798ffa5eb69066d5b1d4
Sean Blake <sean.blake97@outlook.com>
2021-10-24 23:18:26 -0400

    Add the logic to the camera.

--- SevenMiles/Controllers/Core/CameraViewController.swift
+++ SevenMiles/Controllers/Core/CameraViewController.swift
@@ -4,14 +4,176 @@
 //
 //  Created by SEAN BLAKE on 10/8/21.
 //
-
+import AVFoundation
 import UIKit
 
 class CameraViewController: UIViewController {
+    
+    // Catpure settings
+    var captureSession = AVCaptureSession()
+    
+    // Capture Device
+    var videoCaptureDevice: AVCaptureDevice?
+    
+    // Capture Output
+    var captureOutput = AVCaptureMovieFileOutput()
+    
+    // Capture Preview
+    var capturePreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    // the view
+    private let cameraView: UIView = {
+        let view =  UIView()
+        view.clipsToBounds = true
+        view.backgroundColor = .black
+         
+        return view
+    }()
+    
+    // MARK: - Global
+    
+    private var recordingButton =  RecordButton()
+    
+    private var previewLayer: AVPlayerLayer?
+    
+    var recordedVideoURL: URL?
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(cameraView)
         view.backgroundColor = .systemBackground
+        setupCamera()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(didTapCloseBTN))
+        
+        // record button
+        view.addSubview(recordingButton)
+    }
+     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        cameraView.frame = view.bounds
+        let size: CGFloat = 65
+        recordingButton.frame = CGRect(x: (view.width - size)/2, y: view.height - view.safeAreaInsets.bottom - size - 5, width: size, height: size)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        tabBarController? .tabBar.isHidden = true
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func didTapRecord() {
+        if captureOutput.isRecording {
+            // stop recording
+            recordingButton.toggle(for: .notRecording)
+            captureOutput.stopRecording()
+        }
+        else {
+            guard var url = FileManager.default.urls(
+                for: .documentDirectory,
+                   in: .userDomainMask
+            ).first else {
+                return
+            }
+            
+            url.appendPathComponent("video.mov")
+            
+            // toggle record on/off
+            recordingButton.toggle(for: .recording)
+            
+            // Delete if video exist already
+            try? FileManager.default.removeItem(at: url)
+            
+            captureOutput.startRecording(to: url, recordingDelegate: self)
+        }
+    }
+    
+    @objc func didTapCloseBTN() {
+        navigationItem.rightBarButtonItem = nil
+        recordingButton.isHidden = false
+        if previewLayer != nil {
+            previewLayer?.removeFromSuperlayer()
+            previewLayer = nil
+        }
+        else {
+            captureSession.stopRunning()
+            tabBarController?.tabBar.isHidden = false
+            tabBarController?.selectedIndex = 0
+        }
+        
+    }
+    
+    @objc func didTapNext() {
+        
+    }
+    
+    func setupCamera() {
+        // Add devices
+        if let audioDevice = AVCaptureDevice.default(for: .audio) {
+            let audioInput = try? AVCaptureDeviceInput(device: audioDevice)
+            if let audioInput = audioInput {
+                if captureSession.canAddInput(audioInput) {
+                    captureSession.addInput(audioInput )
+                }
+            }
+        }
+        if let videoDevice = AVCaptureDevice.default(for: .video) {
+            if let videoInput = try?  AVCaptureDeviceInput(device: videoDevice) {
+                if captureSession.canAddInput(videoInput) {
+                    captureSession.addInput(videoInput )
+                }
+            }
+        }
+        
+        // Update session
+        captureSession.sessionPreset = .hd1280x720
+        if captureSession.canAddOutput(captureOutput ) {
+            captureSession.addOutput(captureOutput)
+        }
+        
+        // Configure the preview
+        capturePreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        capturePreviewLayer?.videoGravity = .resizeAspectFill
+        capturePreviewLayer?.frame = view.bounds
+        if let layer = capturePreviewLayer {
+            cameraView.layer.addSublayer(layer)
+        }
+        
+        // Enable Camera start
+        captureSession.startRunning()
+    }
+}
+
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+         
+        guard error == nil else {
+            let alert = UIAlertController(title: "YO!!!", message: "Something went wrong.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            present(alert, animated: true)
+            return
+        }
+        
+        recordedVideoURL = outputFileURL
+        
+        // Navigation Next button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .done, target: self, action: #selector(didTapNext))
+        
+        // print("Finish recording video: \(outputFileURL.absoluteString )")
+        // layer ontop of camera view
+        let player = AVPlayer(url: outputFileURL)
+        previewLayer = AVPlayerLayer(player: player)
+        previewLayer?.videoGravity = .resizeAspectFill
+        previewLayer?.frame = cameraView.bounds
+        guard let previewLayer = previewLayer else {
+            return
+        }
+        recordingButton.isHidden = true
+        cameraView.layer.addSublayer(previewLayer)
+        previewLayer.player?.play()
     }
 }
