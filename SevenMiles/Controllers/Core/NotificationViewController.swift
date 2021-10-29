afa43c5932837c72f0993885abeae01ffde91992
Sean Blake <sean.blake97@outlook.com>
2021-10-29 16:38:05 -0400

    Add spinner

--- SevenMiles/Controllers/Core/NotificationViewController.swift
+++ SevenMiles/Controllers/Core/NotificationViewController.swift
@@ -7,11 +7,67 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let noNotificationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.text = "There are no notifications"
+        label.isHidden = true
+        label.textAlignment = .center
+        return label
+    }()
+    /// TABLEVIEW TO SHOW LIST OF NOTIFICATION
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    // Spinner
+    private let spinner: UIActivityIndicatorView = {
+       let spinner = UIActivityIndicatorView()
+        
+        return spinner
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.addSubview(noNotificationLabel)
+        view.addSubview(tableView)
         view.backgroundColor = .systemBackground
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        fetchNotification()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        noNotificationLabel.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        noNotificationLabel.center = view.center
+    }
+    
+    // to fecth notification
+    func fetchNotification() {
+        
+    }
+    
+    // Table View
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Wha Gwaan"
+        return cell
     }
 }
